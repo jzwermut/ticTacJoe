c576092f8fa679fae346b6222e86cb1aa2544603
Jonathan Wermut <jonathanwermut@Jonathans-MBP.modem>
2021-10-06 15:52:06 +1100

    can now change joes and reset the game

--- js/dom.js
+++ js/dom.js
@@ -1,32 +1,61 @@
+let avatar1Id = "";
+let avatar2Id = "";
+let p1Name = "";
+let p2Name = "";
+
 const checkResult = () => {
 	let check = tictacjoe.checkWinner();
 	if (check === 'p1') {
-		$('h2').text('Player one wins!')
+		$('h2#announcer').text(`${p1Name} wins!`)
 	}
 	else if (check === 'p2') {
-		$('h2').text('Player two wins!')
+		$('h2#announcer').text(`${p2Name} wins!`)
 	}
 	else if (check === 'draw') {
-		$('h2').text('Game Over. It\'s a draw')
+		$('h2#announcer').text('Game Over. It\'s a draw')
 	}
 }
 
-const makeChoice = (choice) => {
+const setNameP1 = () => {
+	let name = $('input#p1Name').val();
+	$('#player1 h3').text(name);
+	p1Name = name;
+}
+
+const setNameP2 = () => {
+	let name = $('input#p2Name').val();
+	$('#player2 h3').text(name);
+	p2Name = name;
+}
+
+const getId = (element) => {
+	return element.target.id;
+}
+
+const setJoe = (element) => {
+	let src = element.target.src 
+	// element.currentTarget.attributes[1].nodeValue;
+	return src;
+}
+
+const makeChoice = (choice, id1 = avatar1Id, id2 = avatar2Id) => {
 	let currentTurn = tictacjoe.turn();
 	let result = tictacjoe.choose(`${choice}`);
 		if (result === 'Already selected') {
-			$('h2').text('Already selected. Choose another square');
+			$('h2#announcer').text('Already selected. Choose another square');
 			}
 		else {
 			if (currentTurn === 'player1') {
-				$(`#${choice}`).addClass('murray');
-				$('h2').text('Nice choice')
+				$(`#${choice}`).css("background-image", `url(\'images/${id1}.jpeg\')`)
+				// addClass('murray');
+				$('h2#announcer').text('Nice choice')
 				$('#player1').removeClass('yourturn')
 				$('#player2').addClass('yourturn')
 			 }
 			else {
-				$(`#${choice}`).addClass('cage');
-				$('h2').text('Nice choice')
+				$(`#${choice}`).css("background-image", `url(\'images/${id2}.jpeg\')`)
+				// addClass('cage');
+				$('h2#announcer').text('Nice choice')
 				$('#player1').addClass('yourturn')
 				$('#player2').removeClass('yourturn')
 			}
@@ -34,6 +63,46 @@ const makeChoice = (choice) => {
 }
 
 $(document).ready(function() {
+	$('section.setp2Name').addClass('hideSection');
+	$('section#setJoe').addClass('hideSection');
+	$('#turn').addClass('hideSection');
+	$('section#gameBoard').addClass('hideSection');
+	$('.playAgain').addClass('hideSection');
+	$('.changeJoes').addClass('hideSection');
+
+	$('button#p1Name').on('click', function() {
+		setNameP1();
+		$('section.setp1Name').addClass('hideSection');
+		$('section.setp2Name').removeClass('hideSection');
+		$('#turn').removeClass('hideSection');
+	})
+
+	$('button#p2Name').on('click', function() {
+		setNameP2();
+		$('section.setp2Name').addClass('hideSection');
+		$('section#setJoe').removeClass('hideSection');
+		$('section.setp2Joe').addClass('hideSection');
+	})
+
+	$('section.setp1Joe img').on('click', function(element) {
+		avatar1Id = getId(element);
+		let src = setJoe(element);
+		$('.who#player1 img').attr('src', `${src}`);
+		$('section.setp1Joe').addClass('hideSection');
+		$('section.setp2Joe').removeClass('hideSection');
+
+	})
+
+		$('section.setp2Joe img').on('click', function(element) {
+		avatar2Id = getId(element);	
+		let src = setJoe(element);
+		$('.who#player2 img').attr('src', `${src}`);
+		$('section#setJoe').addClass('hideSection');
+		$('section#gameBoard').removeClass('hideSection');
+		$('.playAgain').removeClass('hideSection');
+		$('.changeJoes').removeClass('hideSection');
+	})
+
 	$('#player1').addClass('yourturn');
 
 	$(`#top-left`).on('click', function() {
@@ -82,9 +151,25 @@ $(document).ready(function() {
 		checkResult();
 	})
 
-	$('button').on('click', function() {
+	$('button.playAgain').on('click', function() { //resets the game
 		tictacjoe.reset();
-		$('.square').removeClass('cage');
-		$('.square').removeClass('murray');
+		$('.square').css("background-image", "");
+		$('#player1').addClass('yourturn')
+		$('#player2').removeClass('yourturn')
 	})
+
+	$('button.changeJoes').on('click', function() { //resets the game
+		tictacjoe.reset();
+		$('.square').css("background-image", "");
+		$('#player1').addClass('yourturn')
+		$('#player2').removeClass('yourturn')
+		$('section#gameBoard').addClass('hideSection');
+		$('.playAgain').addClass('hideSection');
+		$('.changeJoes').addClass('hideSection');
+		$('section#setJoe').removeClass('hideSection');
+		$('section.setp1Joe').removeClass('hideSection');
+		$('section.setp2Joe').addClass('hideSection');
+		$('h2#announcer').text('');
+	})
+
 })
